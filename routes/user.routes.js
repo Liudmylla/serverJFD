f2495c625e05281fa0b32a8c8e9f45cd6ecf2050
Liudmylla <liudmyla12@yahoo.fr>
2022-02-07 18:59:06 +0100

    Middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,35 @@
 const express = require("express");
-
+const User = require("../models/User");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
+
+router.patch("/:userId", auth, async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error, try later",
+    });
+  }
+});
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const list = await User.find();
+    res.send(list);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error, try later",
+    });
+  }
+});
 
 module.exports = router;
